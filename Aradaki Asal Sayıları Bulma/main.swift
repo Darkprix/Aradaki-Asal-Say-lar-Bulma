212a16f97cbe72761e8bb8fe9aec943dc12ef507
Darkprix <yunusemre4365@icloud.com>
2024-11-23 22:01:19 +0300

    main.swift

--- Aradaki Asal Sayıları Bulma/main.swift
+++ Aradaki Asal Sayıları Bulma/main.swift
@@ -7,5 +7,46 @@
 
 import Foundation
 
-print("Hello, World!")
+// Kullanıcıdan 2 tane sayı değeri alacağız
+//Bu iki sayı arasındaki asal sayı değerlerini bulup ekrana yazdıracağız
+
+func aradakiAsalSayılarıBul(begin : Int, end : Int) {
+    for sorgulananSayi in begin...end {
+        for deneyiciSayi in 2...(begin - 1){
+            if sorgulananSayi % deneyiciSayi == 0 {
+                break
+            }else {
+                asalSayilarKumesi.append(sorgulananSayi)
+                break
+            }
+        }
+    }
+}
+
+func cıktıOkuma() {
+    print("\n-----Aradaki Asal Sayılar-----")
+    for (num, cıktılar) in asalSayilarKumesi.enumerated() {
+        print("\(num + 1).sayı:\(cıktılar) ")
+    }
+}
+//Main
+var asalSayilarKumesi : [Int] = []
+print("-----Aradaki asal sayıları bulma programı-----")
+while true {
+    print("Başlangıç değerini giriniz = ")
+    if let begin = Int(readLine() ?? "") {
+        print("Bitiş değerini giriniz = ")
+        if let end = Int(readLine() ?? "") {
+            aradakiAsalSayılarıBul(begin: begin, end: end)
+        }else {
+            print("Lütfen sayı giriniz")
+        }
+    }else {
+        print("Lütfen sayı giriniz")
+    }
+
+    cıktıOkuma()
+}
+
+
 
